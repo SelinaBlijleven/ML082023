ea1f132e805baceda172236be1ebbe27b3d614bc
Lina Blijleven <selinablijleven@gmail.com>
2023-08-28 16:08:15 +0200

    Made updates to the plots and removed the project settings from version control

--- diabetes.py
+++ diabetes.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 """
+An exploration of a diabetes dataset.
+
 Created on Mon Aug 28 09:08:54 2023
 
 @author: (Se)lina Blijleven
 """
 #%% Imports
-
 # This cell can hold all of our imports, so we only have to run them once.
+
+# NUMerical PYthon for statistics and mathematical operations
+import numpy as np
 
 # Pandas for DataFrames and Series (importing as pd is convention)
 import pandas as pd
+
+# Seaborn for visualization
+import seaborn as sns
+sns.set_theme()
+sns.set_palette("viridis")
+
+# Scikit Learn: Learning library
+from sklearn.preprocessing import StandardScaler
 
 #%% Load data
 
@@ -19,3 +31,34 @@ diabetes = pd.read_csv('diabetes.csv')
 # Print the names of the columns we have in our DataFrame.
 print(diabetes.columns)
 
+#%% Pre-processing
+# Removing zero/NaN (because this data is unusable and skews our results)
+db_ppd = diabetes[(diabetes.Glucose != 0) & (diabetes.BloodPressure != 0)]
+
+# Scaling
+db_scaled = pd.DataFrame(
+    StandardScaler().fit_transform(db_ppd), 
+    columns = db_ppd.columns
+)
+
+#%% Exploration: Uni-variate
+# Univariate distributions
+for column in diabetes.columns:
+    
+    # Plot a distribution of the 
+    sns.displot(db_scaled, x=column, binwidth=1)
+
+#%% Exploration 2: Multi-variate
+
+# Determine the correlation matrix between variables
+corr_mat = db_scaled.corr()
+
+sns.heatmap(corr_mat, cmap='coolwarm', annot=True)
+
+#%% Fitting
+
+#%% Evaluation
+
+#%% Fine-tuning
+
+#%% Prediction
