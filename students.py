d9005ea1eb246d824b692fbbcf39f358702ae8b6
Lina Blijleven <selinablijleven@gmail.com>
2023-08-31 10:48:47 +0200

    Paper

---
+++ students.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+students.py
+
+Created on Thu Aug 31 10:31:48 2023
+
+@author: 
+"""
+
+#%% Imports
+
+import pandas as pd
+
+
+#%% Load data
+
+studenten = pd.read_csv('Data/student_data.csv')
